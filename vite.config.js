fcdb5e4d84906a9f10a51bc8c3db016d38e5f584
optimus-the-marsian <nessysaye@gmail.com>
2024-11-30 15:40:01 -0700

    more fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// vite.config.js
-export default {
+export default defineConfig({
+  plugins: [react()],
   server: {
-    port: process.env.PORT || 5173, // Use Heroku's $PORT or fallback to 4173
+    port: process.env.PORT || 5173, // Use Heroku's $PORT or fallback to 5173
     host: '0.0.0.0', // This makes it listen on all network interfaces
   },
-};
-
-
+});
