583047b191437a4d02da3a47f50ca023f3a5b7d0
optimus-the-marsian <nessysaye@gmail.com>
2024-11-30 15:17:14 -0700

    added dynamic port for vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 4173,  // Use Heroku's dynamic PORT or default to 4173
+    host: '0.0.0.0', // Allow access from external network (necessary for Heroku)
+  },
 })
+
+
